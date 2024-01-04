890314656b8c8b90e03254f3b0a0a7edb7880172
jehonathan09 <jonathanmwenge64@gmail.com>
2024-01-04 10:58:47 -0800

    correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node 
 
 const fs = require('fs');
+
 fs.writeFile(process.argv[2], process.argv[3], error => {
-	if (error) console.log(error);
+    if (error) {
+        console.error("Error writing to the file:", error);
+    } else {
+        console.log("File written successfully.");
+    }
 });
+
